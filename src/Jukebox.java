e26ee801e7a27bcab09269c0589b7c2547711c3d
DylanArnold101 <league@iMac-5.attlocal.net>
2019-03-10 11:31:40 -0700

    hsh
    song
    
    !

--- src/Jukebox.java
+++ src/Jukebox.java
@@ -3,7 +3,6 @@
  *    Level 1
  */
 
-
 import java.awt.Button;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -12,7 +11,10 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import javazoom.jl.player.advanced.AdvancedPlayer;
@@ -26,68 +28,70 @@ public class Jukebox implements Runnable {
 		SwingUtilities.invokeLater(new Jukebox());
 	}
 
-           public void run() {
+	public void run() {
 
 		// 3. Find an mp3 on your computer or on the Internet.
-        	   
+
 		// 4. Create a Song
-        	    Song instance = new Song("Three Beat Slide - Journey (Official Music Video) (1).mp3") ; 
-        	    Song fun = new Song("National Anthem of USSR.mp3");
-        	    Song disco = new Song("Abba - Dancing Queen.mp3");
-        	    Song history = new Song("Boney M.- Rasputin.mp3");
-        	    Song napoleon = new Song("Abba - Waterloo.mp3");
-        	    Song classic = new Song("a-ha - Take On Me (Official Music Video) (1).mp3");
-        	    Song Missisipi = new Song("Culture Club - Karma Chameleon (Official Video).mp3");
-        	    Song Life = new Song("Dead Or Alive - You Spin Me Round (Like a Record) (Official Video).mp3");
-        	    Song elements = new Song("Earth, Wind & Fire - September (Official Music Video).mp3");
-        	    Song rabbit = new Song("Echo and the Bunnymen - The Killing Moon (Official Music Video) (1).mp3");
-        	    	Song newtown = new Song("Lipps Inc - Funky Town (1).mp3");
-        	    Song rap = new Song("MC Hammer - U Can't Touch This.mp3");
-        	    Song asia = new Song("Murray Head - One Night In Bangkok From CHESS 2.mp3");
-        	    Song eight = new Song("Rick Astley - Never Gonna Give You Up (Video).mp3");
-        	    Song russia = new Song("Dschinghis Khan - Moskau 1979.mp3");
-        	    Song black = new Song("ACDC - Back In Black (Official Music Video).mp3");
-        	    Song hell = new Song("ACDC - Highway to Hell (Official Video).mp3");
-        	    Song shook = new Song("ACDC - You Shook Me All Night Long (Official Video).mp3");
-        	    Song wake = new Song("Wham! - Wake Me Up Before You Go-Go (Official Music Video).mp3");
-        	    Song Rave = new Song("Noisestorm - Crab Rave [Monstercat Release] (1).mp3");
-        	    // 5. Play the Song
-        	     
-        	    disco.play();
-        	    
-        	    
+		Song instance = new Song("Three Beat Slide - Journey (Official Music Video) (1).mp3");
+		Song fun = new Song("National Anthem of USSR.mp3");
+		Song disco = new Song("Abba - Dancing Queen.mp3");
+		Song history = new Song("Boney M. - Rasputin.mp3");
+		Song napoleon = new Song("Abba - Waterloo.mp3");
+		Song classic = new Song("a-ha - Take On Me (Official Music Video) (1).mp3");
+		Song Missisipi = new Song("Culture Club - Karma Chameleon (Official Video).mp3");
+		Song Life = new Song("Dead Or Alive - You Spin Me Round (Like a Record) (Official Video).mp3");
+		Song elements = new Song("Earth, Wind & Fire - September (Official Music Video).mp3");
+		Song rabbit = new Song("Echo and the Bunnymen - The Killing Moon (Official Music Video) (1).mp3");
+		Song newtown = new Song("Lipps Inc - Funky Town (1).mp3");
+		Song rap = new Song("MC Hammer - U Can't Touch This.mp3");
+		Song asia = new Song("Murray Head - One Night In Bangkok From CHESS 2.mp3");
+		Song eight = new Song("Rick Astley - Never Gonna Give You Up (Video).mp3");
+		Song russia = new Song("Dschinghis Khan - Moskau 1979.mp3");
+		Song black = new Song("ACDC - Back In Black (Official Music Video).mp3");
+		Song hell = new Song("ACDC - Highway to Hell (Official Video).mp3");
+		Song shook = new Song("ACDC - You Shook Me All Night Long (Official Video).mp3");
+		Song wake = new Song("Wham! - Wake Me Up Before You Go-Go (Official Music Video).mp3");
+		Song Rave = new Song("Noisestorm - Crab Rave [Monstercat Release] (1).mp3");
+
+		// 5. Play the Song
+
+		shook.play();
+
 		/*
-		 * 6. Create a user interface for your Jukebox so that the user can to
-		 * choose which song to play. You can use can use a different button for
-		 * each song, or a picture of the album cover. When the button or album
-		 * cover is clicked, stop the currently playing song, and play the one
-		 * that was selected.
+		 * 6. Create a user interface for your Jukebox so that the user can to choose
+		 * which song to play. You can use can use a different button for each song, or
+		 * a picture of the album cover. When the button or album cover is clicked, stop
+		 * the currently playing song, and play the one that was selected.
 		 */
-        	 Button instanceB = new Button("Three Beat Slide - Journey (Official Music Video) (1).mp3");
-        	 Button funN = new Button("National Anthem of USSR.mp3");
-       	 Button discoB = new Button("Abba - Dancing Queen.mp3");
-       	 Button historyB = new Button("Boney M.- Rasputin.mp3");
-       	 Button napoleonB = new Button("Abba - Waterloo.mp3");
-       	 Button classicB = new Button("a-ha - Take On Me (Official Music Video) (1).mp3");
-       	 Button MissisipiB = new Button("Culture Club - Karma Chameleon (Official Video).mp3");
-       	 Button LifeB = new Button("Dead Or Alive - You Spin Me Round (Like a Record) (Official Video).mp3");
-       	 Button elementsB = new Button("Earth, Wind & Fire - September (Official Music Video).mp3");
-       	 Button rabbitB = new Button("Echo and the Bunnymen - The Killing Moon (Official Music Video) (1).mp3");
-       	 Button newtownB = new Button("Lipps Inc - Funky Town (1).mp3");
-       	 Button rapB = new Button("MC Hammer - U Can't Touch This.mp3");
-       	 Button asiaB = new Button("Murray Head - One Night In Bangkok From CHESS 2.mp3");
-       	 Button eightB = new Button("Rick Astley - Never Gonna Give You Up (Video).mp3");
-       	 Button russiaB = new Button("Dschinghis Khan - Moskau 1979.mp3");
-       	 Button blackB = new Button("ACDC - Back In Black (Official Music Video).mp3");
-       	 Button  hellB = new Button("ACDC - Highway to Hell (Official Video).mp3");
-       	 Button shookB = new Button("ACDC - You Shook Me All Night Long (Official Video).mp3");
-        	 Button wakeB = new Button("Wham! - Wake Me Up Before You Go-Go (Official Music Video).mp3");
-        	   
-          }
+		JPanel button = new JPanel();
+		JFrame forbuttons = new JFrame();
+		JButton instanceB = new JButton("Three Beat Slide - Journey (Official Music Video) (1).mp3");
+		JButton funN = new JButton("National Anthem of USSR.mp3");
+		JButton discoB = new JButton("Abba - Dancing Queen.mp3");
+		JButton historyB = new JButton("Boney M. - Rasputin.mp3");
+		JButton napoleonB = new JButton("Abba - Waterloo.mp3");
+		JButton classicB = new JButton("a-ha - Take On Me (Official Music Video) (1).mp3");
+		JButton MissisipiB = new JButton("Culture Club - Karma Chameleon (Official Video).mp3");
+		JButton LifeB = new JButton("Dead Or Alive - You Spin Me Round (Like a Record) (Official Video).mp3");
+		JButton elementsB = new JButton("Earth, Wind & Fire - September (Official Music Video).mp3");
+		JButton rabbitB = new JButton("Echo and the Bunnymen - The Killing Moon (Official Music Video) (1).mp3");
+		JButton newtownB = new JButton("Lipps Inc - Funky Town (1).mp3");
+		JButton rapB = new JButton("MC Hammer - U Can't Touch This.mp3");
+		JButton asiaB = new JButton("Murray Head - One Night In Bangkok From CHESS 2.mp3");
+		JButton eightB = new JButton("Rick Astley - Never Gonna Give You Up (Video).mp3");
+		JButton russiaB = new JButton("Dschinghis Khan - Moskau 1979.mp3");
+		JButton blackB = new JButton("ACDC - Back In Black (Official Music Video).mp3");
+		JButton hellB = new JButton("ACDC - Highway to Hell (Official Video).mp3");
+		JButton shookB = new JButton("ACDC - You Shook Me All Night Long (Official Video).mp3");
+		JButton wakeB = new JButton("Wham! - Wake Me Up Before You Go-Go (Official Music Video).mp3");
+		
+	}
+
 	private void Jlabel(String string) {
-			// TODO Auto-generated method stub
-			
-		}
+		// TODO Auto-generated method stub
+
+	}
 
 	/* Use this method to add album covers to your Panel. */
 	private JLabel loadImage(String fileName) {
@@ -106,8 +110,7 @@ class Song {
 	private InputStream songStream;
 
 	/**
-	 * Songs can be constructed from files on your computer or Internet
-	 * addresses.
+	 * Songs can be constructed from files on your computer or Internet addresses.
 	 * 
 	 * Examples: <code> 
 	 * 		new Song("everywhere.mp3"); 	//from default package 
@@ -182,4 +185,3 @@ class Song {
 		}
 	}
 }
-
